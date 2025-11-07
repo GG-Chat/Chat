edc529a63b970433c475b4284cc996d9285768d8
Granxy Global Chat <eonwuasoanya78@gmail.com>
2025-11-07 07:42:50 +0100

    Update config.php

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 // Update the password in this file after upload:
 $host = "sql7.freesqldatabase.com";
 $user = "sql7806479";
-$pass = "YOUR_DB_PASSWORD_HERE"; // <-- replace with @Gallant666 in your IDE
+$pass = "@Gallant666"; // <-- replace with @Gallant666 in your IDE
 $dbname = "sql7806479";
 
 $conn = new mysqli($host, $user, $pass, $dbname);
